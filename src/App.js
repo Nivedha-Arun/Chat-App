ebb501b23da7df49992e9d64ef2743b379c7992b
Nivedha <nivedhaprakash29@gmail.com>
2021-05-11 15:30:04 +0530

    superops-ai assessment

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import { useEffect, useState } from 'react';
+import './App.scss';
+import Card from './components/Card.js'
+import {FaBars} from "react-icons/fa"
+
+let pageToken, pageId = "";
+var newList = [];
+
+function App() {
+
+  const [error, setError] = useState(null);
+  const [items, setItems] = useState([]);
+  const [page, setPageToken] = useState([]);
+
+  useEffect(() => {
+  fetch(`http://message-list.appspot.com/messages${pageId}`)
+    .then(res => res.json())
+    .then(
+      (result) => {
+        pageToken = result.pageToken;
+        Array.prototype.push.apply(newList,result.messages); 
+        setItems(newList);
+      },
+      (error) => {
+        setError(error);
+      }
+    )
+
+}, [page]);
+
+const scrollToEnd = () => {
+  setPageToken(pageId = "?pageToken="+pageToken)
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+window.onscroll = function() {
+  if(window.innerHeight + document.documentElement.scrollTop === document.documentElement.offsetHeight)
+  {
+    scrollToEnd();
+  }
+}
+
+
+  return (
+    <div className="App">
+      <div className="header">
+        <span className="bars"><FaBars /></span>
+        <h3>Messages</h3>
+      </div>
+      <div className="content">
+      {items.map((dataItem) => 
+      { 
+      return ( 
+          <Card key = {dataItem.id} data = {dataItem}/> 
+          ); 
+      })}
+      </div>
+      </div>
+  );
+}
+
+export default App;
---
+++ src/components/Card.js
@@ -0,0 +1,68 @@
+import React from "react";
+import {FaTrash} from "react-icons/fa"
+
+var prop = "";
+
+function Card(props) {
+prop = props;
+var photoUrl = "http://message-list.appspot.com"+props.data.author.photoUrl;
+
+var postDate = new Date(props.data.updated);
+var currentDate = new Date();
+var differenceInDate = currentDate.getFullYear() - postDate.getFullYear();
+
+const touchMove = (event) => {
+    var element = document.getElementsByClassName("main-content");
+    var id= ((props.data.id-1) % 10);
+    element[id].classList.add("touchMove");
+};
+
+const touchStart = (event) => {
+    var element = document.getElementsByClassName("container");
+    var deleteAction = document.getElementsByClassName("deleteAction");
+    var id= ((props.data.id-1) % 10);
+    deleteAction[id].classList.add("visible");
+    deleteAction[id].style.height = element[id].offsetHeight - 25 + "px";
+    touchMove();
+}
+
+const buttonClick = () => {
+    var element = document.getElementsByClassName("main-content");
+    var deleteAction = document.getElementsByClassName("deleteAction");
+    var id= ((props.data.id-1) % 10);
+    element[id].classList.remove("touchMove");
+    deleteAction[id].classList.remove("visible");
+}
+
+    return (
+        <div className="actions" id={props.data.id}>          
+        <div className="container" id={props.data.id} onTouchMove={touchMove} onTouchStart={touchStart}>
+        <button className="deleteAction" onClick={buttonClick}><span className="bars">Delete</span>
+        <span className="trash"><FaTrash /></span></button>
+            <div className="main-content">
+                <div className="user-details">
+                <img src={photoUrl} alt={props.data.author.name} />
+                <div className="details">
+                <h1>{props.data.author.name}</h1>
+                <h2>{differenceInDate} years ago</h2>
+                </div>
+            </div>
+            <div className="message">
+                <h3>{props.data.content}</h3>
+            </div>
+        </div>
+        </div>
+        </div>
+    )
+}
+
+var classname = document.getElementsByClassName("container");
+
+function handleGestureMove(){
+    console.log("Swiped");
+  }
+
+if(prop)
+classname[prop.data.id].addEventListener('touchmove', handleGestureMove, false)
+
+export default Card;
